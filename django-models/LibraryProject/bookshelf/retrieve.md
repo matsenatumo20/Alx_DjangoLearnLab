e9818c63b06056230492f8898c87ca202407053b
Tumo Matsena <matsenatumo20@gmail.com>
2025-08-08 22:25:11 +0200

    Delete django-models/LibraryProject directory

--- django-models/LibraryProject/bookshelf/retrieve.md
+++
@@ -1,9 +0,0 @@
-# Create Operation
-
-## Command
-python
-book = Book.objects.get(title="1984")
-print(book.title, book.author, book.published_date)
-
-*Output*
-1984 George Orwell 1949
--- django-models/LibraryProject/relationship_app/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import list_books, LibraryDetailView
-
-urlpatterns = [
-    path('books/', views.list_books, name='list_books'),
-    path('libraries/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
-]
