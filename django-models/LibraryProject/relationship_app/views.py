8e43ebb86b778a0a00d6adae6d783d38c073d966
Tumo Matsena <matsenatumo20@gmail.com>
2025-08-09 03:01:49 +0200

    Project 2 commit

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Book
 
-# Create your views here.
+def list_books(request):
+    books = Book.objects.select_related('author').all()
+    
+    if not books:
+        return HttpResponse("No books found.")
+
+    output = "Books in Database:\n\n"
+    for book in books:
+        output += f"- {book.title} by {book.author.name}\n"
+
+    return HttpResponse(output, content_type="text/plain")
+
+
+from django.shortcuts import render, get_object_or_404
+from django.views.generic.detail import DetailView
+from .models import Book, Library
+
+# Function-Based View: List all books
+def list_books(request):
+    books = Book.objects.select_related('author').all()
+    return render(request, 'relationship_app/list_books.html', {'books': books})
+
+# Class-Based View: Library detail view with books
+class LibraryDetailView(DetailView):
+    model = Library
+    template_name = 'relationship_app/library_detail.html'
+    context_object_name = 'library'
+
