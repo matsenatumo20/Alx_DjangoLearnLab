290c33316befff2b9e2d08cfa97bdd4b2b86e453
Tumo Matsena <matsenatumo20@gmail.com>
2025-08-09 19:39:32 +0200

    Update views.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.views.generic.detail import DetailView
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required, user_passes_test, permission_required
+from django.contrib.auth.decorators import permission_required
 
 from .models import Book, Library
 from .forms import BookForm
