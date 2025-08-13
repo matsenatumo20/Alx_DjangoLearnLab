b09c993033bd5fead92a8117400e5f08f234c4ed
Tumo Matsena <matsenatumo20@gmail.com>
2025-08-14 00:49:31 +0200

    Update urls.py

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -5,7 +5,9 @@ urlpatterns = [
     path('books/', BookListView.as_view(), name='book_list'),
     path('books/create/', BookCreateView.as_view(), name='book_create'),
     path('books/<int:pk>/', BookDetailView.as_view(), name='book_detail'),
-    path('books/<int:pk>/update/', BookUpdateView.as_view(), name='book_update'),
-    path('books/<int:pk>/delete/', BookDeleteView.as_view(), name='book_delete'),
+
+    # These contain the exact strings "books/update" and "books/delete"
+    path('books/update/<int:pk>/', BookUpdateView.as_view(), name='book_update'),
+    path('books/delete/<int:pk>/', BookDeleteView.as_view(), name='book_delete'),
 ]
 
