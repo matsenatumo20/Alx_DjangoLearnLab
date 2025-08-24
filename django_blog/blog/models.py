0aa222698fca39a5e41896031ae05e2eb0d1f18b
Tumo Matsena <matsenatumo20@gmail.com>
2025-08-25 01:19:06 +0200

    Update models.py

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -2,6 +2,7 @@ from django.conf import settings
 from django.db import models
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Post(models.Model):
