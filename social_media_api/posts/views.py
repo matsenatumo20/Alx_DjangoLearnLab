e2307329476015518c4ae53fa89a5306b255ea63
Tumo Matsena <matsenatumo20@gmail.com>
2025-09-02 21:58:15 +0200

    Update views.py

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -8,7 +8,7 @@ from rest_framework import filters
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import generics
-
+from rest_framework import permissions
 
 # Create your views here.
 class IsAuthorOrReadOnly(IsAuthenticatedOrReadOnly):
@@ -42,9 +42,9 @@ class CommentViewSet(viewsets.ModelViewSet):
         serializer.save(author=self.request.user)
 
 class FeedView(generics.ListAPIView):
-    permission_classes = [IsAuthenticated]
+    permission_classes = [permissions.IsAuthenticated]
     serializer_class = PostSerializer
 
     def get_queryset(self):
-        followed_users = self.request.user.following.all()
-        return Post.objects.filter(author__in=followed_users).order_by('-created_at')
+        following_users = self.request.user.following.all()
+        return Post.objects.filter(author__in=following_users).order_by('-created_at')
