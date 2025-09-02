5f2e3d47e7f257d393cef1e25b3ae1955e630005
Tumo Matsena <matsenatumo20@gmail.com>
2025-09-02 20:27:09 +0200

    Update views.py

--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -4,16 +4,14 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.authtoken.models import Token
-from .serializers import UserSerializer
-from .models import User
+from .serializers import RegisterSerializer
 
 # Create your views here.
 class RegisterView(APIView):
     def post(self, request):
-        serializer = UserSerializer(data=request.data)
+        serializer = RegisterSerializer(data=request.data)
         if serializer.is_valid():
-            user = serializer.save()
-            token = Token.objects.create(user=user)
+            user, token = serializer.save()
             return Response({'token': token.key}, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
